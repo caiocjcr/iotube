1042f33b0cfdb06e18d5b3bf7a19f5448b4f287d
Caio Rosa <caio.cjcr@gmail.com>
2022-06-22 23:17:21 -0300

    chore: add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Header } from '../components'
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+export type AppTheme = {
+  background: string
+  primary: string
+  secondary: string
+  text: string
+  border: string
+}
+
+const theme: AppTheme = {
+  background: '#331E36',
+  primary: '#3E738E',
+  secondary: '#594A54',
+  text: '#ECFEE8',
+  border: 'rgba(255, 255, 255 0.1)',
+}
+
+export default theme
