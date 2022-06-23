b88845a3ae1da8e0d1288bedb967f109eb981622
Caio Rosa <caio.cjcr@gmail.com>
2022-06-22 23:36:28 -0300

    feat: add button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classnames from 'classnames'
+import { StyledButton } from './button.styles'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'outlined' | 'destructive'
+  fullWidth?: boolean
+}
+
+const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  fullWidth = false,
+  className,
+  children,
+  ...rest
+}) => {
+  const buttonClassNames = classnames({
+    [variant]: true,
+    'full-w': fullWidth,
+    [className ?? '']: !!className,
+  })
+  return (
+    <StyledButton className={buttonClassNames} {...rest}>
+      {children}
+    </StyledButton>
+  )
+}
+
+export default Button
