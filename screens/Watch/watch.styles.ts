9cb5cd0d5f65f903bf64a3f07dcc91b79c3b6805
Caio Rosa <caio.cjcr@gmail.com>
2022-06-23 22:12:23 -0300

    fix: set correct watch page element widths

--- screens/Watch/watch.styles.ts
+++ screens/Watch/watch.styles.ts
@@ -9,10 +9,11 @@ export const WatchWrapper = styled.div`
 
 export const InfoWrapper = styled.div`
   width: clamp(192px, 100vw, 680px);
-  aspect-ratio: 16 / 9;
 `
 
 export const VideoContainer = styled.div`
+  aspect-ratio: 16 / 9;
+  width: clamp(192px, 100vw, 680px);
   iframe {
     width: 100%;
     height: 100%;
