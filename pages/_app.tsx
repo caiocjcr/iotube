60ef731e8bf215a559a0b3734df9a66fd5e5dfa4
Caio Rosa <caio.cjcr@gmail.com>
2022-06-25 16:06:47 -0300

    chore: add react-query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,20 @@ import type { AppProps } from 'next/app'
 import { Header } from '@/components'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/styles/theme'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
