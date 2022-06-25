77e22da5abf46b83e17b7bdc0ba3c421b2c2aced
Caio Rosa <caio.cjcr@gmail.com>
2022-06-25 16:30:01 -0300

    feat: add infinite scroll functionality to search page

--- screens/Search/Search.tsx
+++ screens/Search/Search.tsx
@@ -6,24 +6,26 @@ import { useInfiniteQuery } from 'react-query'
 import { searchVideos } from '@/services/youtubeApi'
 import { FoundVideo, SearchVideosResponse } from '@/types'
 import { useMemo } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const SearchPage: React.FC<SearchPageProps> = (initialSearch) => {
-  const { data } = useInfiniteQuery<SearchVideosResponse>(
-    'searchResults',
-    ({ pageParam: pageToken = undefined }) =>
-      searchVideos({ q: '', pageToken }),
-    {
-      initialData: {
-        pages: [initialSearch],
-        pageParams: [],
-      },
-      getNextPageParam: (lastPage) => lastPage.nextPageToken,
-      getPreviousPageParam: (lastPage) => lastPage.prevPageToken,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-    }
-  )
+  const { data, hasNextPage, fetchNextPage } =
+    useInfiniteQuery<SearchVideosResponse>(
+      'searchResults',
+      ({ pageParam: pageToken = undefined }) =>
+        searchVideos({ q: '', pageToken }),
+      {
+        initialData: {
+          pages: [initialSearch],
+          pageParams: [],
+        },
+        getNextPageParam: (lastPage) => lastPage.nextPageToken,
+        getPreviousPageParam: (lastPage) => lastPage.prevPageToken,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+      }
+    )
 
   const videos = useMemo(
     () =>
@@ -36,15 +38,22 @@ const SearchPage: React.FC<SearchPageProps> = (initialSearch) => {
 
   return (
     <SearchWrapper>
-      {videos.map((video) => (
-        <VideoSnippetContainer key={video.id.videoId}>
-          <Link href={`/watch?v=${video.id.videoId}`} passHref>
-            <a>
-              <VideoSnippet video={video} />
-            </a>
-          </Link>
-        </VideoSnippetContainer>
-      ))}
+      <InfiniteScroll
+        dataLength={videos.length}
+        hasMore={!!hasNextPage}
+        next={fetchNextPage}
+        loader={<div>loading...</div>}
+      >
+        {videos.map((video) => (
+          <VideoSnippetContainer key={video.id.videoId}>
+            <Link href={`/watch?v=${video.id.videoId}`} passHref>
+              <a>
+                <VideoSnippet video={video} />
+              </a>
+            </Link>
+          </VideoSnippetContainer>
+        ))}
+      </InfiniteScroll>
     </SearchWrapper>
   )
 }
