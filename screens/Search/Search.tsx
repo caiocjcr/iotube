02f18dfd574c7679c05e1595b21c3fd240474bf5
Caio Rosa <caio.cjcr@gmail.com>
2022-06-25 16:13:53 -0300

    feat: use react query hook to manage search result pages

--- screens/Search/Search.tsx
+++ screens/Search/Search.tsx
@@ -2,8 +2,38 @@ import { VideoSnippet } from '@/components'
 import { SearchPageProps } from '@/pages/search'
 import Link from 'next/link'
 import { SearchWrapper, VideoSnippetContainer } from './search.styles'
+import { useInfiniteQuery } from 'react-query'
+import { searchVideos } from '@/services/youtubeApi'
+import { FoundVideo, SearchVideosResponse } from '@/types'
+import { useMemo } from 'react'
 
-const SearchPage: React.FC<SearchPageProps> = ({ videos }) => {
+const SearchPage: React.FC<SearchPageProps> = (initialSearch) => {
+  const { data } = useInfiniteQuery<SearchVideosResponse>(
+    'searchResults',
+    ({ pageParam: pageToken = undefined }) =>
+      searchVideos({ q: '', pageToken }),
+    {
+      initialData: {
+        pages: [initialSearch],
+        pageParams: [],
+      },
+      getNextPageParam: (lastPage) => lastPage.nextPageToken,
+      getPreviousPageParam: (lastPage) => lastPage.prevPageToken,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    }
+  )
+
+  const videos = useMemo(
+    () =>
+      data?.pages.reduce(
+        (prev: FoundVideo[], current) => [...prev, ...current.items],
+        []
+      ) ?? [],
+    [data]
+  )
+
   return (
     <SearchWrapper>
       {videos.map((video) => (
