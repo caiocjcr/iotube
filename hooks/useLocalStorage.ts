39f526535fee63d25641a6ab34d50080a739c5c9
Caio Rosa <caio.cjcr@gmail.com>
2022-06-27 16:05:04 -0300

    feat: verify if there is a stored value before initializing

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,10 +1,13 @@
 import { Dispatch, SetStateAction, useState } from 'react'
 
-const useLocalStorage = <T>(
+type StorableTypes = string | number | Array<unknown> | Record<string, unknown>
+
+const useLocalStorage = <T extends StorableTypes>(
   localStorageKey: string,
   initialValue: T
 ): [T, Dispatch<SetStateAction<T>>] => {
-  const [state, setState] = useState<T>(initialValue)
+  const stored = localStorage.getItem(localStorageKey)
+  const [state, setState] = useState<T>((stored as T) || initialValue)
 
   return [state, setState]
 }
