1f050d8c0305e337bd0fd22f2f14f850455e83ef
Caio Rosa <caio.cjcr@gmail.com>
2022-06-28 15:05:29 -0300

    fix: set desktop padding

--- screens/Home/home.styles.ts
+++ screens/Home/home.styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const HomeWrapper = styled.div`
   width: clamp(192px, 1580px, 100vw);
   margin: 0 auto;
+
+  @media (min-width: 768px) {
+    padding: 0 16px;
+  }
 `
