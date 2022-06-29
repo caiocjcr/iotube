1196932003aed0fa5c0805772637400600d76537
Caio Rosa <caio.cjcr@gmail.com>
2022-06-29 12:39:35 -0300

    chore: set app page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,24 +6,30 @@ import theme from '@/styles/theme'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { UserHistoryProvider } from '@/contexts'
 import LoadingIndicatorProvider from '@/contexts/loading-indicator'
+import Head from 'next/head'
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <LoadingIndicatorProvider indicateOnPageLoad>
-          <LoadingBar />
-          <UserHistoryProvider>
-            <Header />
-            <main>
-              <Component {...pageProps} />
-            </main>
-          </UserHistoryProvider>
-        </LoadingIndicatorProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>ioTube</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <LoadingIndicatorProvider indicateOnPageLoad>
+            <LoadingBar />
+            <UserHistoryProvider>
+              <Header />
+              <main>
+                <Component {...pageProps} />
+              </main>
+            </UserHistoryProvider>
+          </LoadingIndicatorProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </>
   )
 }
 
